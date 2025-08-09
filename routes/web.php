86b002b7e70d1c60ddf2b918130a5e269397b031
hack-entertainer <hack.entertainer@gmail.com>
2025-08-08 23:17:35 -0400

    bridged laravel 7-8 project config, renamed conflicting web route

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,9 @@
     Route::get('user/register', [FrontendController::class, 'register'])->name('register.form');
     Route::post('user/register', [FrontendController::class, 'registerSubmit'])->name('register.submit');
 // Reset password
-    Route::post('password-reset', [FrontendController::class, 'showResetForm'])->name('password.reset');
-// Socialite
+    // Route::post('password-reset', [FrontendController::class, 'showResetForm'])->name('password.reset');
+    Route::post('password-reset', [FrontendController::class, 'showResetForm'])->name('frontend.password.reset');
+    // Socialite
     Route::get('login/{provider}/', [LoginController::class, 'redirect'])->name('login.redirect');
     Route::get('login/{provider}/callback/', [LoginController::class, 'Callback'])->name('login.callback');
 
