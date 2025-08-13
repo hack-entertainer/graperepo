b3b94ea7959ef4742d40b775e47452a47f5327c9
Hack-Ent <hack.entertainer@gmail.comC>
2025-08-13 01:56:33 -0400

    fixed another route error

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -65,7 +65,7 @@
             <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
             Profile
           </a>
-          <a class="dropdown-item" href="{{route('user.change.password.form')}}">
+         <a class="dropdown-item" href="{{ route('user.password.change.form') }}">
             <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
             Change Password
           </a>
