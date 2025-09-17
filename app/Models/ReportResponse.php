a508ac1c415c9d0e7d560e4435855bc00f064fb2
Hack-Ent <hack.entertainer@gmail.comC>
2025-09-16 20:25:52 -0400

    linkking report response to original report by id

--- app/Models/ReportResponse.php
+++ app/Models/ReportResponse.php
@@ -7,17 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReportResponse extends Model
 {
-    use HasFactory;
+	use HasFactory;
 
-    protected $fillable = [
-        'user_id',
-        'report_id',
-        'user_fullname',
-        'type',
-        'content',
-        'file_path',
-        'is_paid',
-        'payment_status',
-        'paid_at',
-    ];
+	protected $fillable = [
+		'user_id',
+		'report_id',
+		'user_fullname',
+		'type',
+		'content',
+		'file_path',
+		'is_paid',
+		'payment_status',
+		'paid_at',
+	];
+
+	public function report()
+	{
+		return $this->belongsTo(Reports::class, 'report_id');
+	}
 }
