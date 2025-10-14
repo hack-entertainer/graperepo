047a78ad37847d7e035beb6a63e37e718bf89269
Hack-Ent <hack.entertainer@gmail.comC>
2025-10-13 20:15:46 -0400

    formatting

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,90 +1,90 @@
 <header class="header shop">
-    <!-- Topbar -->
-    <div class="topbar">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-12 col-12">
-                    <!-- Top Left -->
-                    <div class="top-left">
-                        <ul class="list-main">
-                            @php
-                                $settings=DB::table('settings')->get();
-                                
-                            @endphp
-                            <li><i class="ti-headphone-alt"></i>@foreach($settings as $data) {{$data->phone}} @endforeach</li>
-                            <li><i class="ti-email"></i> @foreach($settings as $data) {{$data->email}} @endforeach</li>
+	<!-- Topbar -->
+	<div class="topbar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-12 col-12">
+					<!-- Top Left -->
+					<div class="top-left">
+						<ul class="list-main">
+							@php
+							$settings=DB::table('settings')->get();
 
-                        </ul>
-                    </div>
-                    <!--/ End Top Left -->
-                </div>
-                <div class="col-lg-6 col-md-12 col-12">
-                    <!-- Top Right -->
-                    <div class="right-content">
-                        <ul class="list-main">
-                            @auth 
-                                @if(Auth::user()->role=='admin')
-                                    <li><i class="ti-user"></i> <a href="{{route('admin')}}"  target="_blank">Dashboard</a></li>
-                                @else 
-                                    <li><i class="ti-user"></i> <a href="{{route('user')}}"  target="_blank">Dashboard</a></li>
-                                @endif
-                                <li><i class="ti-power-off"></i> <a href="{{route('user.logout')}}">Logout</a></li>
+							@endphp
+							<li><i class="ti-headphone-alt"></i>@foreach($settings as $data) {{$data->phone}} @endforeach</li>
+							<li><i class="ti-email"></i> @foreach($settings as $data) {{$data->email}} @endforeach</li>
 
-                            @else
-                                <li><i class="ti-power-off"></i><a href="{{route('login.form')}}">Login /</a> <a href="{{route('register.form')}}">Register</a></li>
-                            @endauth
-                        </ul>
-                    </div>
-                    <!-- End Top Right -->
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- End Topbar -->
-    <div class="middle-inner">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-2 col-md-2 col-12">
-                    <!-- Logo -->
-                    <div class="logo">
-                        @php
-                            $settings=DB::table('settings')->get();
-                        @endphp                    
-                        <a href="{{route('home')}}"><img src="@foreach($settings as $data) {{$data->logo}} @endforeach" alt="logo"></a>
-                    </div>
-                    <!--/ End Logo -->
-                    
-                </div>
-                
-            </div>
-        </div>
-    </div>
-    <!-- Header Inner -->
-    <div class="header-inner">
-        <div class="container">
-            <div class="cat-nav-head">
-                <div class="row">
-                    <div class="col-lg-12 col-12">
-                        <div class="menu-area">
-                            <!-- Main Menu -->
-                            <nav class="navbar navbar-expand-lg">
-                                <div class="navbar-collapse">	
-                                    <div class="nav-inner">	
-                                        <ul class="nav main-menu menu navbar-nav">
-                                            <li class="{{Request::path()=='home' ? 'active' : ''}}"><a href="{{route('home')}}">Home</a></li>
-                                            <!-- <li class="{{Request::path()=='about-us' ? 'active' : ''}}"><a href="{{route('about-us')}}">About Us</a></li> -->
-                                            <li class=""><a href="{{route('list-reports')}}">List reports</a><span class="new">New</span></li>												
-                                            <li class="{{Request::path()=='post-rules' ? 'active' : ''}}"><a href="{{route('post-rules')}}">Post Rules and more detail</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </nav>
-                            <!--/ End Main Menu -->	
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--/ End Header Inner -->
+						</ul>
+					</div>
+					<!--/ End Top Left -->
+				</div>
+				<div class="col-lg-6 col-md-12 col-12">
+					<!-- Top Right -->
+					<div class="right-content">
+						<ul class="list-main">
+							@auth
+							@if(Auth::user()->role=='admin')
+							<li><i class="ti-user"></i> <a href="{{route('admin')}}" target="_blank">Dashboard</a></li>
+							@else
+							<li><i class="ti-user"></i> <a href="{{route('user')}}" target="_blank">Dashboard</a></li>
+							@endif
+							<li><i class="ti-power-off"></i> <a href="{{route('user.logout')}}">Logout</a></li>
+
+							@else
+							<li><i class="ti-power-off"></i><a href="{{route('login.form')}}">Login /</a> <a href="{{route('register.form')}}">Register</a></li>
+							@endauth
+						</ul>
+					</div>
+					<!-- End Top Right -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Topbar -->
+	<div class="middle-inner">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-2 col-12">
+					<!-- Logo -->
+					<div class="logo">
+						@php
+						$settings=DB::table('settings')->get();
+						@endphp
+						<a href="{{route('home')}}"><img src="@foreach($settings as $data) {{$data->logo}} @endforeach" alt="logo"></a>
+					</div>
+					<!--/ End Logo -->
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- Header Inner -->
+	<div class="header-inner">
+		<div class="container">
+			<div class="cat-nav-head">
+				<div class="row">
+					<div class="col-lg-12 col-12">
+						<div class="menu-area">
+							<!-- Main Menu -->
+							<nav class="navbar navbar-expand-lg">
+								<div class="navbar-collapse">
+									<div class="nav-inner">
+										<ul class="nav main-menu menu navbar-nav">
+											<li class="{{Request::path()=='home' ? 'active' : ''}}"><a href="{{route('home')}}">Home</a></li>
+											<!-- <li class="{{Request::path()=='about-us' ? 'active' : ''}}"><a href="{{route('about-us')}}">About Us</a></li> -->
+											<li class=""><a href="{{route('list-reports')}}">List reports</a><span class="new">New</span></li>
+											<li class="{{Request::path()=='post-rules' ? 'active' : ''}}"><a href="{{route('post-rules')}}">Post Rules and more detail</a></li>
+										</ul>
+									</div>
+								</div>
+							</nav>
+							<!--/ End Main Menu -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/ End Header Inner -->
 </header>
