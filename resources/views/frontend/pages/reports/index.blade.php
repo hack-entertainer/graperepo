424f0410909b11d2e875767a3dc9be77aa1250c2
Hack-Ent <hack.entertainer@gmail.comC>
2025-10-14 23:14:24 -0400

    broke table out correctly

--- resources/views/frontend/pages/reports/index.blade.php
+++ resources/views/frontend/pages/reports/index.blade.php
@@ -45,55 +45,7 @@
 
 		<div class="table-responsive">
 			@if(($reports && $reports->count()) || ($answers && $answers->count()) || ($comments && $comments->count()))
-			<table class="table table-striped table-bordered align-middle text-center table-hover">
-				<thead class="table-dark">
-					<tr>
-						<th>Report #</th>
-						<th>Reporter</th>
-						<th>Subject</th>
-						<th>Type</th>
-						<th>Location</th>
-						<th>Date Event</th>
-						<th>Date of Report</th>
-						<th>Video</th>
-						<th>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach($reports as $report)
-					<tr>
-						<td><a href="{{route('report-detail',$report->report_number)}}" style="color: #007bff;" title="View details">{{$report->report_number}}</a></td>
-						<td><strong>{{$report->reporter_name}}</strong></td>
-						<td>{{$report->subject_fullname}}</td>
-
-						<td>{{$report->type_event}}</td>
-						<td>{{$report->event_location}}</td>
-						<td>{{$report->event_date}}</td>
-						<td>{{$report->created_at}}</td>
-						<td>
-							@if($report->video_link)
-							<a href="{{$report->video_link}}" target="_blank" style="color:#007bff" title="View video">
-								<i class="fa fa-video-camera" aria-hidden="true"></i>
-							</a>
-							@else
-							--
-							@endif
-						</td>
-						<td><a href="{{route('report-detail',$report->report_number)}}" class="btn btn-primary btn-sm" title="View details"> Detail</a></td>
-					</tr>
-					@endforeach
-					{{-- Existing Reports loop --}}
-
-					@if(request()->filled('q')
-					&& (!$reports || !$reports->count())
-					&& (!$answers || !$answers->count())
-					&& (!$comments || !$comments->count()))
-					<p>No results found.</p>
-					@endif
-
-				</tbody>
-
-			</table>
+			@include('frontend.pages.reports.components.table')
 
 			{{-- New: Answers --}}
 			@if(isset($answers) && $answers->count())
