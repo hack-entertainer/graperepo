0ca3d4903b6518e34abf4e7f926cf2623c4f58e6
Juan Rodriguez <hack.entertainer@gmail.com>
2026-01-09 16:00:24 -0500

    removed diagnostic files

--- public/_pdo.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-
-echo "PHP: " . PHP_VERSION . PHP_EOL;
-echo "PDO class exists: ";
-var_dump(class_exists('PDO'));
-echo "PDO extension loaded: ";
-var_dump(extension_loaded('pdo'));
