5c32b091ecaa98bd5b846c392a6f8c567eefd44d
Juan Rodriguez <hack.entertainer@gmail.com>
2026-01-11 21:46:27 -0500

    correctly implementing db seeders

---
+++ database/seeders/Users/AdminUsersSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\Users;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
